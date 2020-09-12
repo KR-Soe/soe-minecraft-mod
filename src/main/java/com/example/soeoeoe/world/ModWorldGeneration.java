b95211b5753aa69255c28f427740df59700b4eac
KR-Soe <swoordeath@gmail.com>
2020-09-12 05:24:13 -0300

    nerfed orichalcum ore generation

--- src/main/java/com/example/soeoeoe/world/ModWorldGeneration.java
+++ src/main/java/com/example/soeoeoe/world/ModWorldGeneration.java
@@ -22,10 +22,10 @@ public class ModWorldGeneration implements IWorldGenerator{
 	}
 	
 	private void generateOverWorld(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
-		int commonOreChances = 20;
+		int commonOreChances = 16;
 		int rareOreChances = 13;
 		int legendaryOreChances = 7; 
-		generateOres(BlockInit.ORICHALCUM_BLOCK.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 6, 80, random.nextInt(5) + 2, commonOreChances);
+		generateOres(BlockInit.ORICHALCUM_BLOCK.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 6, 40, random.nextInt(5) + 2, commonOreChances);
 		generateOres(BlockInit.LUNITA_BLOCK.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 6, 14, random.nextInt(4) + 1, rareOreChances);
 	}
 	
